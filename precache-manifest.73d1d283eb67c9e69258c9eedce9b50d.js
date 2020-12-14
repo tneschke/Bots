67ef6ca11b4f0b66e41ba30881fe7ec062463b68
Teague Neschke <tneschke@gmail.com>
2020-12-14 10:41:48 -0600

    Updates

---
+++ precache-manifest.73d1d283eb67c9e69258c9eedce9b50d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adbee688486e236d1b36a0e7c29fb0f",
+    "url": "/Bots/index.html"
+  },
+  {
+    "revision": "1e028025baa1c575c5f8",
+    "url": "/Bots/static/css/2.12a7ee3a.chunk.css"
+  },
+  {
+    "revision": "785f6678d351e45ffba5",
+    "url": "/Bots/static/css/main.85b247a9.chunk.css"
+  },
+  {
+    "revision": "1e028025baa1c575c5f8",
+    "url": "/Bots/static/js/2.462a382c.chunk.js"
+  },
+  {
+    "revision": "785f6678d351e45ffba5",
+    "url": "/Bots/static/js/main.8233c8df.chunk.js"
+  },
+  {
+    "revision": "7184201a14b533669c06",
+    "url": "/Bots/static/js/runtime-main.20d7a246.js"
+  }
+]);
